131d467baa20b10af323118a1250ca456ed855a4
Abhinav Mangla <55951300+mangla-abhinav@users.noreply.github.com>
2020-06-14 18:02:51 +0530

    Fixed Bugs

--- assets/js/Tic_Tac_Toe.js
+++ assets/js/Tic_Tac_Toe.js
@@ -11,6 +11,11 @@ $('.square').css({
     'height': cw + 'px'
 });
 
+var iw = $('.square').width();
+$('.square i').css({
+    'font-size': iw + 'px'
+});
+
 var squares = $(".square");
 
 $("#developer").on("click", function () {
